9aa6581490cf76e3870ff7adb6bb55c8131cb9e9
Prabhjit Singh <pj92singh@gmail.com>
2018-02-03 20:37:20 -0500

    Add files via upload

---
+++ docs/assets/js/Simple-Slider.js
@@ -0,0 +1,11 @@
+$(function(){
+
+
+	var mySwiper = new Swiper ('.swiper-container', {
+		loop: true,
+		pagination: '.swiper-pagination',
+		paginationClickable: true,
+		nextButton: '.swiper-button-next',
+		prevButton: '.swiper-button-prev'
+	});
+}); 
