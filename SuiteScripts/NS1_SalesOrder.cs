5ab42017ecea9b6f0d497ec47bb760a01d36619d
Prabhjit Singh <pj92singh@gmail.com>
2020-01-16 23:01:08 -0500

    Update NS1_SalesOrder.cs

--- SuiteScripts/NS1_SalesOrder.cs
+++ SuiteScripts/NS1_SalesOrder.cs
@@ -9,7 +9,8 @@ namespace ConsoleApp4
         * Prabhjit Singh 
         * 
         * using class for NS related record creation 
-        * can work with webservices request 
+        * can work with webservices request with either using the POSTman test
+        * or the Suiteanswers connector test
         */
         public class SalesOrder
         {
@@ -39,6 +40,17 @@ namespace ConsoleApp4
             };
             Console.WriteLine($"The SalesOrder is created for: {SalesOrder.Entity} {SalesOrder.Subsidiary.ToUpper()}");
             Console.ReadKey(); 
+            }
+             /* connection creating function
+             -using TSTDRV account to create the sales order
+             -first create the connection 
+             -then send over the data for the SalesOrder
+             */
+            public static void createConnection()
+            {
+               //using admin information 
+              // and suitetalk endpoint
+                TODO connector information test
             }
         }
  }
