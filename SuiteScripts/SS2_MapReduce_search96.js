70a707e20c022719bfe6f71935684930223c2246
Prabhjit Singh <pj92singh@gmail.com>
2024-03-08 11:58:57 -0500

    Update SS2_MapReduce_search96.js

--- SuiteScripts/SS2_MapReduce_search96.js
+++ SuiteScripts/SS2_MapReduce_search96.js
@@ -6,39 +6,47 @@
 * Version: 1.0.0
 *
 *
-* Author: pj92singh
+* Author: prabhjitsinghdev
 Prabhjit Singh 
 
 * Purpose: Map/reduce script example 
--referring to search 96 in test account 
+-referring to search parameter in test account 
 -then retrive related fields and return appropriate data
 *
 *
 * ******************************************************************* */
 
 /**
- *@NApiVersion 2.x
+ *@NApiVersion 2.1
  *@NScriptType MapReduceScript 
  */
 //2nd map reduce for custom 15 saved serach 
- define(['N/search'], 
- 	function(search){
+ define(['N/search'], (search) => {
+ 	const getInputData = (context) => {
+        try {
+            const script = runtime.getCurrentScript();
+            const params = {
+                search_id: script.getParameter('custscript_fbn_mr_ach_search'),
+                max_records: script.getParameter('custscript_fbn_mr_ach_max_records')
+            };
+            log.debug({ title: 'DEBUG search', details: params.search_id });
+            if (isEmpty(params.search_id)) {
+                log.error({ title: 'ERROR missing search', details: `Search is required.` });
+                return;
+            }
+            const s = search.load({
+                id: params.search_id
+            });
 
- 		function getInputData(){
+            return s;
 
- 			return{
- 				type: 'search',
- 				id: 96
- 		}
- 		}
- 		function map(context){
- 			log.debug({ 
- 				title: 'DEBUG',
- 				details: context
- 				});
-			
-
- 			//key stuff
+        } catch (e) {
+            log.error({ title: 'ERROR getInputData', details: e });
+        }
+    }
+    const map = (context) => {
+        try{
+            //key stuff
  			var key = context.key;
  			var recobj = JSON.parse(context.value); //gettting object
  			var trandate = recobj.values.trandate;
@@ -53,36 +61,6 @@ Prabhjit Singh
  			});
  			//now break that down more
  			var salesrep = recobj.values.salesrep; 
- 			log.debug({ 
- 				title: 'DEBUG',
- 				details: 'salesrepName: ' +salesrepName
- 			});
- 			log.debug({ 
- 				title: 'DEBUG',
- 				details: 'salesrepID: ' +salesrepID 				
- 			});
- 			log.debug({ 
- 				title: 'DEBUG',
- 				details: 'trandate: ' +trandate
- 			});
-
-			log.debug({ 
- 				title: 'DEBUG',
- 				details: 'transactionnumber: ' +transactionnumber
- 			});
-
-			log.debug({ 
- 				title: 'DEBUG',
- 				details: 'salesrepSupID: ' +salesrepSupID
- 			});
-			
-			log.debug({ 
- 				title: 'DEBUG',
- 				details: 'salesrepSupName: ' +salesrepSupName
- 			});
- 			
- 			
-
  			//now try to get supevisor's id
  			//values.values.supervisor.text??
  			var supervisorName = recobj.values.supervisor.text; 
@@ -99,21 +77,15 @@ Prabhjit Singh
  				//with this line
  				//the sales orders under your name are: " "
 
+        }catch(e){
+            log.error({ title:'ERROR map', details: e });
+        }
+ 			
 
- 		}
- 		function reduce(context){
- 			log.debug({ 
- 				title: 'DEBUG',
- 				details: 'reduce: ' +context
- 			});
+ 	}
 
-
- 		}
-
- 		return{
- 			getInputData: getInputData,
- 			map: map,
- 			reduce: reduce
- 		}
- 		
- });
+ 	return{
+ 		getInputData,
+ 		map
+ 	}
+});
