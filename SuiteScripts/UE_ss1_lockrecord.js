5715c430f464a38ab36ce26c5f94c9999964f900
Prabhjit Singh <pj92singh@gmail.com>
2024-03-10 23:23:26 -0400

    updating jsdoc tags

--- SuiteScripts/UE_ss1_lockrecord.js
+++ SuiteScripts/UE_ss1_lockrecord.js
@@ -6,7 +6,7 @@
 * Version: 1.0.0
 *
 *
-* Author: pj92singh
+* Author: prabhjitsinghdev
 Prabhjit Singh 
 
 * Purpose: this script removes the edit button 
@@ -15,27 +15,28 @@ Prabhjit Singh
 *
 *
 * ******************************************************************* */
+/**
+*@copyright 2024 
+*@author prabhjitsinghdev
+*/
 
-
-function beforeload(context){
-		if(type == 'view'){
-			if(nlapiGetRole()!= 3){
-				form.removeButton('edit');
-				nlapiLogExecution('DEBUG', 'removebutton', 'done');
-			}
+function beforeload(context) {
+	if (type == 'view') {
+		if (nlapiGetRole() !== 3) {
+			form.removeButton('edit');
+			nlapiLogExecution('DEBUG', 'removebutton', 'done');
 		}
-		else if(type == 'edit'){
-			//if the user clicks EDIT in the list view
-			//we check this here and then proceed or not allow them 
-			if(nlapiGetRole()== 3){
-				nlapiLogExecution('DEBUG', 'equals 3', 'continue');
+	}
+	else if (type == 'edit') {
+		//if the user clicks EDIT in the list view
+		//we check this here and then proceed or not allow them 
+		if (nlapiGetRole() == 3) {
+			nlapiLogExecution('DEBUG', 'equals 3', 'continue');
 
-			}else{
-				form.removeButton('save');
-				alert('You are not Administrator; closing window');
-				setTimeout(function () { window.close();}, 5000); 
-			}
-
-
+		} else {
+			form.removeButton('save');
+			alert('You are not Administrator; closing window');
+			setTimeout(function () { window.close(); }, 5000);
 		}
+	}
 }
