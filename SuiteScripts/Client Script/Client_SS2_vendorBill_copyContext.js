87cccfff9de3b8ba1ca259a70406bf1364c2c663
Prabhjit Singh <pj92singh@gmail.com>
2024-04-23 16:55:21 -0400

    Update Client_SS2_vendorBill_copyContext.js

--- SuiteScripts/Client Script/Client_SS2_vendorBill_copyContext.js
+++ SuiteScripts/Client Script/Client_SS2_vendorBill_copyContext.js
@@ -3,7 +3,7 @@
 *
 * Name: Client_SS2_vendorBill_copyContext.js
 * Script Type: ClientScript
-* Version: 0.0.1
+* Version: 0.0.2
 *
 *
 * Author: Prabhjit Singh
@@ -14,41 +14,46 @@
 * **** if bill is not stand alone then the system treats it as a copy 
 * so the script will execute in that context as well ****
 * ******************************************************************* */
- /**
- * @NApiVersion 2.x
- *@NScriptType ClientScript
- *
- */
- define(['N/record', 'N/currentRecord', 'N/ui/message', 'N/ui/dialog'],
-	function(record, currentRecord, message, dialog){
-		function pageInit(context){
-			log.debug({title: 'DEBUG', details: 'CLIENT SS2' +'context ' +context +'JSON ' +JSON.stringify(context)}); 
-			if(context.mode == 'create'){	
-					//get record?!?!	
-					//
-					//new record
-					var recID = record.id;
-					var recType = record.type;
-                    log.debug({title: 'DEBUG', details: 'id//type: ' +recID +' // ' +recType}); 
-                     //alert-------------------------------
-                        dialog.alert({
-                            title: 'Alert',
-                            message: 'being made originally!' 
-                        });				
-        }
-        //in copy mode!!! 
-        if(context.mode =='copy'){
-            log.debug({title: 'DEBUG', details: 'copy mode'}); 
-            //alert-------------------------------
-            dialog.alert({
-                title: 'Alert',
-                message: 'being made in copy!' 
-            });
-            //alert--end--------------------------
-        }
-	}
-		return{
-			pageInit: pageInit
+/**
+* @NApiVersion 2.x
+*@NScriptType ClientScript
+*
+*/
+define(['N/record', 'N/currentRecord', 'N/ui/message', 'N/ui/dialog'],
+    function (record, currentRecord, message, dialog) {
+        function pageInit(context) {
+            let acceptedContexts = ["create", "edit"]; 
+            log.debug({ title: 'DEBUG', details: 'CLIENT SS2' + 'context ' + context + 'JSON ' + JSON.stringify(context) });
+            let curRec = currentRecord.get();
+            if (acceptedContexts.includes(context.mode)) {
+                //new record
+                var recID = record.id;
+                var recType = record.type;
+                log.debug({ title: 'DEBUG', details: 'id//type: ' + recID + ' // ' + recType });
+                //alert-------------------------------
+                dialog.alert({
+                    title: 'Alert',
+                    message: 'Record is being created or edited!'
+                });
+            }
+            //in copy mode!!! 
+            if (!acceptedContexts.includes(context.mode)) {
+                log.debug({ title: 'DEBUG', details: 'copy mode' });
+                //alert-------------------------------
+                dialog.alert({
+                    title: 'Alert',
+                    message: 'Record created as a copy!'
+                });
+                //alert--end--------------------------
 
-		}
-	});
+                /* Other logic to setup when record is a copy */
+                curRec.setValue({ fieldId: 'memo', value: `this record is being created as a copy and is will require admin approval` });
+                curRec.setValue({ fieldId: 'custbody_ecm_multitext', value: [3,5,8] });
+                curRec.setValue({ fieldId: 'custbody_ecm_checkbox', value: true }); 
+            }
+        }
+        return {
+            pageInit: pageInit
+
+        }
+    });
