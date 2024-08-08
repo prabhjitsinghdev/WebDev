ec9078a853bfa2ea3c2f6abb84323241db6345ee
Prabhjit Singh <pj92singh@gmail.com>
2024-08-07 21:52:20 -0400

    Update snippet_getSublistInfo.js

--- SuiteScripts/Snippets/snippet_getSublistInfo.js
+++ SuiteScripts/Snippets/snippet_getSublistInfo.js
@@ -1,3 +1,14 @@
+/**
+* @author prabhjitsinghdev 
+*
+* There are 2 versions of this snippet 
+* 1) First version will just give a predefined set of sublist values 
+*
+* 2) Second version will take in an arry of field names as a thrid parameter to then return the values of those fields
+*
+*
+**/
+/* ******* VERSION 1 below that takes an array of fld values to run through ********** */
 require(["N/search", "N/currentRecord", "N/ui/dialog", "N/runtime"], (
     search,
     currentRecord,
@@ -58,4 +69,52 @@ require(["N/search", "N/currentRecord", "N/ui/dialog", "N/runtime"], (
     
     
   });
+
+/* ******* VERSION 2 below that takes an array of fld values to run through ********** */
+require(["N/search", "N/currentRecord", "N/ui/dialog", "N/runtime"], (
+    search,
+    currentRecord,
+    dialog,
+    runtime
+) => {
+
+    const getLineItems = (recordObj, sublistName, fldsArr) => {
+        try {
+            const lineItemArr = [];
+            if (!recordObj) { return false; }
+            const lineCount = recordObj.getLineCount({
+                sublistId: sublistName
+            });
+            for (let x = 0; x < lineCount; x++) {
+                let tempObj = {};
+                for (let fld = 0; fld < fldsArr.length; fld++) {
+                    //console.log(typeof fldsArr[fld]);
+                    let pulledValue = recordObj.getSublistValue({
+                        sublistId: sublistName,
+                        fieldId: fldsArr[fld],
+                        line: x
+                    });
+                    tempObj[fldsArr[fld]] = pulledValue;
+                }
+                lineItemArr.push(tempObj);
+                console.log("DEBUG getLineItems", `print obj ${JSON.stringify(tempObj)}`);
+            }
+            console.log("DEBUG getLineItems", `length: ${lineItemArr.length} and lineItemArr ${lineItemArr}`);
+
+            return lineItemArr;
+
+        } catch (error) {
+            console.log({ title: 'ERROR getLineItems', details: error });
+        }
+    }
+
+    console.log('DEBUG getting line information');
+    const cRec = currentRecord.get();
+    getLineItems(cRec, 'item', ['item', 'quantity']);
+
+
+
+});
+
+
   
