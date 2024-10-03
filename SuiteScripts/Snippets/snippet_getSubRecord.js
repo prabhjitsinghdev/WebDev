88edb705c0a32e3edde774c97c066c4747f6edda
Prabhjit Singh <pj92singh@gmail.com>
2024-10-03 16:44:24 -0400

    Update snippet_getSubRecord.js

--- SuiteScripts/Snippets/snippet_getSubRecord.js
+++ SuiteScripts/Snippets/snippet_getSubRecord.js
@@ -37,3 +37,49 @@ let shipaddress = nlapiViewSubrecord('shippingaddress')
 let shipcountry = shipaddress.getFieldText('country');
 console.log(`shipcountry ${shipcountry}`);
 let shipstate = shipaddress.getFieldText('dropdownstate');
+/* *********** ********* */
+
+
+/* inventory detail snippet code */
+require(['N/currentRecord'], (currentRecord)=>{
+    recordObj = currentRecord.get();
+    let sublistName = 'item'; 
+
+    let itemPulled = recordObj.getCurrentSublistValue({
+        sublistId: sublistName,
+        fieldId: 'item'
+    });
+    let locationPulled = recordObj.getCurrentSublistValue({
+        sublistId: sublistName,
+        fieldId: 'location'
+    });
+    console.log('DEBUG setLineItems', `itemPulled ${itemPulled} vs locationPulled ${locationPulled}`); 
+    const invDetailRec = recordObj.getCurrentSublistSubrecord({
+        sublistId: sublistName,
+        fieldId: 'inventorydetail'
+    });
+    console.log('DEBUG invDetail', `invDetailRec ${invDetailRec}`); 
+    if(invDetailRec){
+        invDetailRec.selectLine({
+            sublistId: 'inventoryassignment',
+            line: 0
+        });
+        let binnum = invDetailRec.getValue({
+            sublistId: 'inventoryassignment',
+            fieldId: 'binnumber'
+        });
+        console.log(`binnum ${binnum}`); 
+        let quantity = invDetailRec.getValue({
+            sublistId: 'inventoryassignment',
+            fieldId: 'quantity'
+        });
+        console.log(`quantity ${quantity}`); 
+        let status = invDetailRec.getValue({
+            sublistId: 'inventoryassignment',
+            fieldId: 'toinventorystatus'
+        });
+        console.log(`status ${status}`); 
+
+        toinventorystatus
+    }
+});
