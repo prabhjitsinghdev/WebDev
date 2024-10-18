e48d2e607e29627decbfd1e7251a7f4710da92a1
Prabhjit Singh <pj92singh@gmail.com>
2024-10-18 11:39:00 -0400

    Update tests_Query.sql

--- SQL/tests_Query.sql
+++ SQL/tests_Query.sql
@@ -2,3 +2,8 @@
  SELECT *
 FROM transaction t
  WHERE t.trandate BETWEEN TO_DATE('2024-03-01', 'YYYY-MM-DD') and TO_DATE('2024-03-31', 'YYYY-MM-DD')
+
+
+ SELECT t.id, t.tranid, FROM TRANSACTION t
+ LEFT JOIN transactionLine tl on t.id = tl.transaction and tl.mainLine = 'F' 
+WHERE t.type = 'VendBill' and t.status is IN ('VendBill:A', 'VendBill:D')
