5a210bd8f9d93c83070605f170131d09b420c018
Prabhjit Singh <pj92singh@gmail.com>
2026-06-25 12:18:46 -0400

    Add SQL query for inbound shipment analysis
    Added a complex SQL query to retrieve inbound shipment details.

--- SQL/inbound_shipment.sql
+++ SQL/inbound_shipment.sql
@@ -5,3 +5,48 @@ FROM
 INNER JOIN 
     InboundShipmentItem tl ON tr.id = tl.InboundShipment
 WHERE tr.id = 2338
+
+
+
+SELECT
+    resolved.item_internal_id,
+    BUILTIN.DF(resolved.item_internal_id) AS item_name,
+    SUM(resolved.inbound_quantity)        AS inbound_quantity
+FROM (
+    SELECT
+        candidate.inbound_line_id,
+        MAX(candidate.item_internal_id)   AS item_internal_id,
+        MAX(candidate.inbound_quantity)   AS inbound_quantity
+    FROM (
+        SELECT DISTINCT
+            isi.id                       AS inbound_line_id,
+            tl.item                      AS item_internal_id,
+            NVL(isi.quantityexpected, 0) AS inbound_quantity
+        FROM
+            InboundShipmentItem isi
+        INNER JOIN
+            transactionline tl
+                ON tl.transaction = isi.purchaseordertransaction
+                AND tl.mainline = 'F'
+                AND tl.item IS NOT NULL
+        LEFT JOIN
+            item i
+                ON i.id = tl.item
+        WHERE
+            isi.inboundshipment = ?
+            AND (
+                i.description = isi.shipmentitemdescription
+                OR tl.memo = isi.shipmentitemdescription
+                OR BUILTIN.DF(tl.item) = isi.shipmentitemdescription
+            )
+    ) candidate
+    GROUP BY
+        candidate.inbound_line_id
+    HAVING
+        COUNT(DISTINCT candidate.item_internal_id) = 1
+) resolved
+GROUP BY
+    resolved.item_internal_id,
+    BUILTIN.DF(resolved.item_internal_id)
+ORDER BY
+    BUILTIN.DF(resolved.item_internal_id)
